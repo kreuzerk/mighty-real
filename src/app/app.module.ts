bee46afba71bb0fac7b4e59dd016bb81e3082142
kreuzerk <kevin.kreuzer90@icloud.com>
2023-05-24 16:07:51 +0200

    feat: 🎸 setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'mighty-real',
+    appId: '1:325917094613:web:2b2a24b018c9882801a85c',
+    storageBucket: 'mighty-real.appspot.com',
+    apiKey: 'AIzaSyBqmCZcsLWlCtWPYl32aL-7_Xol-vQLiEw',
+    authDomain: 'mighty-real.firebaseapp.com',
+    messagingSenderId: '325917094613',
+  },};
